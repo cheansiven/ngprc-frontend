d37c7f900aec3865a18615b9209ef807815144ec
Viazul <vannarith@syncmusic.rocks>
2021-09-27 16:06:10 +0700

    update

--- src/components/FrontTopMenu.vue
+++ src/components/FrontTopMenu.vue
@@ -5,22 +5,21 @@
         align="right"
         class="top-menu"
     >
-      <b-nav-item to="/" :active="this.$route.name == 'home'">Home</b-nav-item>
-      <b-nav-item  to="/presentation" :active="this.$route.name == 'presentation'">Presentation</b-nav-item>
-      <b-nav-item  to="/presenter" :active="this.$route.name == 'presenter'">Presenter</b-nav-item>
-      <b-nav-item  to="/conference" :active="this.$route.name == 'conference'">Conferences</b-nav-item>
+      <b-nav-item :to="{name : 'home'}" :active="this.$route.name == 'home'">Home</b-nav-item>
+      <b-nav-item  :to="{name : 'presentation'}" :active="this.$route.name == 'presentation'">Presentation</b-nav-item>
+      <b-nav-item  :to="{name : 'presenter'}" :active="this.$route.name == 'presenter'">Presenter</b-nav-item>
+      <b-nav-item  :to="{name : 'conference'}" :active="this.$route.name == 'conference'">Conferences</b-nav-item>
       <b-nav-item-dropdown
           id="my-nav-dropdown"
           text="Achievements"
           toggle-class="nav-link-custom"
           right
       >
-        <b-dropdown-item>Medias</b-dropdown-item>
-        <b-dropdown-item>Video</b-dropdown-item>
+        <b-dropdown-item :to="{name : 'video'}" :active="this.$route.name == 'video'" >Video</b-dropdown-item>
         <b-dropdown-divider />
-        <b-dropdown-item>Blog</b-dropdown-item>
+        <b-dropdown-item :to="{name : 'blog'}" :active="this.$route.name == 'blog'">Blog</b-dropdown-item>
       </b-nav-item-dropdown>
-      <b-nav-item to="/login">
+      <b-nav-item :to="{name: 'auth-login'}">
         Login
       </b-nav-item>
     </b-nav>
